b26527e2ef0c1f98176e4c7173714a5e33d98f93
Artur Juraszek <asgavar@gmail.com>
2016-12-04 13:56:55 +0100

    added descriptions

--- D1P2.py
+++ D1P2.py
@@ -1,3 +1,14 @@
+# --- Day 1: No Time for a Taxicab ---
+#
+# --- Part Two ---
+#
+# Then, you notice the instructions continue on the back of the Recruiting Document. Easter Bunny HQ is actually at the first location you visit twice.
+#
+# For example, if your instructions are R8, R4, R4, R8, the first location you visit twice is 4 blocks away, due East.
+#
+# How many blocks away is the first location you visit twice?
+
+
 with open("D1.txt") as file:
     instructions = file.read().replace(",", "").split()
 
